2a744695ece52334f06e4295401f712bd17db2b8
bashiru98 <atulebashirubukari@gmail.com>
2021-04-21 03:14:49 +0300

    new commit

--- bigONotation.js
+++ bigONotation.js
@@ -3,26 +3,36 @@ const { performance, PerformanceObserver } = require("perf_hooks")
 const addUpTo = (n) => {
     let total = 0;
     for (let i = 1; i <= n; i++) {
-        total +=i
+        total +=i   // O(n)
     }
     return total;
 }
 
 // much more efficient one
 const addFunction = (n) => {
-    return n * (n + 1) / 2
+    return n * (n + 1) / 2 // O(0)
 }
 
+const printUpPair = (n) => {
+    for ( let i = 0; i < n; i++) {
+        for( let j = 0; j < n; j++) {
+            console.log(i,j)   
+        }
+    }  
+    //  O(n^2)
+}
+printUpPair(10)
 const perfObserver = new PerformanceObserver((items) => {
     items.getEntries().forEach((entry) => {
       console.log(entry)
     })
   })
   
+
   perfObserver.observe({ entryTypes: ["measure"], buffer: true })
 performance.mark("example-start")
 // console.log(addFunction(1000000000000))  // less cpu intensive
-console.log(addUpTo(1000000000000))    // very cpu intensive 
+// console.log(addUpTo(1000000000000))    // very cpu intensive 
 performance.mark("example-end")
 
 performance.measure("example", "example-start", "example-end")
