97c18e5bc451f4b2d758b52041cd7d16fb869dbc
bashiru98 <atulebashirubukari@gmail.com>
2021-04-26 15:38:01 +0300

    update

---
+++ linearSeach.js
@@ -0,0 +1,10 @@
+// basic search used in most js or nodejs 
+
+function linearSearch(arr, val){
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === val) return i;
+    }
+    return -1;
+}
+
+linearSearch([34,51,1,2,3,45,56,687], 100)
